dcc7dd6b0411c7b2af252f2d10468ade480b19ec
Thejasgr141 <thejasgr603@gmail.com>
2025-03-10 15:57:57 +0530

    Create add of two num.py

---
+++ add of two num.py
@@ -0,0 +1,11 @@
+#arthmetic code
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("addition",c)
+print("subtraction",d)
+print("multiplication",e)
+print("devision",f)
