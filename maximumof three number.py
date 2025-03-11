7e9ec8fe756b10f729decac47e5c9b23fb0fb4f9
Thejasgr141 <thejasgr603@gmail.com>
2025-03-11 15:37:51 +0530

    Create maximumof three number.py

---
+++ maximumof three number.py
@@ -0,0 +1,7 @@
+#maximum of three number 
+def max_of_three(a, b, c):
+  return (a,b,c)
+num1=int(input("Enter first number "))
+num2=int(input("Enter second number"))
+num3=int(input("Enter third number"))
+print("The maximum number is:",max_of_three(num1,num2,num3))
