8d29e1a30c9574bb64f956ce8dbea7b34086471f
Thejasgr141 <thejasgr603@gmail.com>
2025-03-11 11:54:12 +0530

    Create greater number.py

---
+++ greater number.py
@@ -0,0 +1,9 @@
+num1= float(input("Enter the first number"))
+num2= float(input("Enter the second number"))
+num3= float(input("Enter the third number"))
+if num1>=num2 and num1>=num3:
+ print("num1 is big")
+elif num2>num3:
+  print("num2 is big")
+else:
+  print("num3 is big")
