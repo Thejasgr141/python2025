f809047a070c11d00b9f44abcf728b523ece3f17
Thejasgr141 <thejasgr603@gmail.com>
2025-03-11 15:16:57 +0530

    Create defaultargument.py

---
+++ defaultargument.py
@@ -0,0 +1,5 @@
+def greet(name, age=18):  # Default age is 18
+    print(f"Hello {name}, you are {age} years old.")
+
+greet("Thejas")  # Uses default age (18)
+greet("Ravi", 20)  # Overrides default age
