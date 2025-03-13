403c076db0ac47d3f2373b1f22b17f6a86d5648d
Thejasgr141 <thejasgr603@gmail.com>
2025-03-13 15:00:12 +0530

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,6 @@
+num=[1,2,3,4,5,6,7,8,9]
+accending_order=sorted(num)
+decending_order=sorted(num,reverse=True)
+print("Original list",num)
+print("accending order",accending_order)
+print("decending order",decending_order)
