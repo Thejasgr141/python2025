397452029c2aba882c3d25d1b76f38692e1007dc
Thejasgr141 <thejasgr603@gmail.com>
2025-03-13 14:48:43 +0530

    Create removeandpop.py

---
+++ removeandpop.py
@@ -0,0 +1,7 @@
+num=[10,20,30,40,50,]
+print("Original list:",num)
+num.remove(30)
+print("list after remove 30",num)
+poped_element=num.pop(2)
+print("least after using pop(2)",num)
+print("poped element",poped_element)
