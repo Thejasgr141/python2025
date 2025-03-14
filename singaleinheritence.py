2e926522545741cf0cee1ed4dd65e1f315c6c090
Thejasgr141 <thejasgr603@gmail.com>
2025-03-14 11:46:00 +0530

    Create singaleinheritence.py

---
+++ singaleinheritence.py
@@ -0,0 +1,8 @@
+class kishore:
+   def love(self):
+    print("I am very handsome guy")
+class Babyy(kishore):
+ def love(self):
+   print("I am vey handsome guy")
+kishore1=Babyy()
+kishore1.love()
